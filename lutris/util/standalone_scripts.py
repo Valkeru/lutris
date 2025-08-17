38c371d0479a437dcf0ae2e15f09ba5b9f7c8943
Igor Kobiakov <valkeru@valkeru.ru>
2025-08-17 18:00:35 +0300

    Add extra arguments support with --extra-args CLI option

--- lutris/util/standalone_scripts.py
+++ lutris/util/standalone_scripts.py
@@ -3,7 +3,7 @@ import os
 from lutris.game import Game
 
 
-def generate_script(logger, launch_ui_delegate, db_game, script_path):
+def generate_script(logger, launch_ui_delegate, db_game, script_path, extra_args):
     """Output a script to a file.
     The script is capable of launching a game without the client
     """
@@ -16,6 +16,15 @@ def generate_script(logger, launch_ui_delegate, db_game, script_path):
     game = Game(db_game["id"])
     game.game_error.register(on_error)
     game.reload_config()
+
+    if extra_args:
+        args = merge_args(game, extra_args)
+        game.config.game_config["args"] = args
+
     game.write_script(script_path, launch_ui_delegate)
     absolutePath = os.path.abspath(script_path)
     logger.info(f"Wrote script to: '{absolutePath}'")
+
+def merge_args(game, extra_args):
+    args = game.config.game_config["args"] + " " + extra_args.get_string()
+    return args
